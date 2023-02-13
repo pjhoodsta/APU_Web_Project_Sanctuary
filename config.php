eade6d8d10d2fb1c802444797c075e7aff56c0e8
Seung OK <pjhoodsta@hotmail.com>
2023-02-13 14:17:11 +0800

    adding database configuration file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+define('DBSERVER', 'localhost');
+define('DBUSERNAME', 'root');
+define('DBPASSWORD', '');
+define('DBNAME', 'demo');
+
+$db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
+
+//Check db connection
+if ($db === false) {
+    die("Error: connection error. ". mysqli_connect_error());
+    # code...
+}
+?>
