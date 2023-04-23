27ac49c3e038a7bd2a8dabc78dee7f90d0ad023d
pjhoodsta <pjhoodsta@hotmail.com>
2023-04-23 08:12:10 +0800

    middle of glasssmorphism. stuck on reading image file for background

--- registration.php
+++ registration.php
@@ -8,9 +8,9 @@
     <link rel="stylesheet" href="style.css" />
     <title>Registration</title>
   </head>
-  <body class="gradient">
+  <body >
     <header>
-      <section class="navbar flex-box-column">
+      <div class="navbar flex-box-column">
         <div class="fillerline"></div>
         <div class="navbar-tooltip">
           <ul class="navbar-nav">
@@ -27,77 +27,75 @@
             <img src="./images/site-logo.png" alt="site-logo" />
           </a>
         </div>
-        <div class="fillerline background-color-white"></div>
-      </section>
-    </header>
-    <main class="gradient">
-      <section class="card registration flex-box-column">
-        <h1>Registration Form</h1>
-        <form action="./registration-post.php" method="POST">
-          <div class="card-item-container card-item-container-registration">
-            <div class="card-item card-item-registration flex-box-column">
-              <label>First Name</label>
-              <input
-                type="text"
-                name="firstName"
-                id="firstName"
-                placeholder="Your name..."
-              />
-            </div>
-            <div class="card-item card-item-registration flex-box-column">
-              <label>Last Name</label>
-              <input
-                type="text"
-                name="lastName"
-                placeholder="Your last name..."
-              />
-            </div>
-          </div>
-          <div class="card-item-container card-item-container-registration">
-            <div class="card-item card-item-registration flex-box-column">
-              <label>Birthday</label>
-              <input type="date" id="birthday" name="birthday" />
-            </div>
 
-            <div class="card-item card-item-registration flex-box-column">
-              <label>Gender</label>
-              <div class="radio-container">
-                <label class="radio-item">
-                  <input type="radio" id="male"  name="gender" checked="checked" />
-                  Male
-                </label>
-                <label class="radio-item">
-                  <input type="radio" id="female"  name="gender" />
-                  Female
-                </label>
+      </div>
+    </header>
+    <main>
+      <div class="card registration flex-box-column">
+          <h1>Registration Form</h1>
+          <form action="./registration-post.php" method="POST">
+            <div class="card-item-container card-item-container-registration">
+              <div class="card-item card-item-registration flex-box-column">
+                <label>First Name</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  placeholder="Your name..."
+                />
+              </div>
+              <div class="card-item card-item-registration flex-box-column">
+                <label>Last Name</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Your last name..."
+                />
               </div>
             </div>
-          </div>
+            <div class="card-item-container card-item-container-registration">
+              <div class="card-item card-item-registration flex-box-column">
+                <label>Birthday</label>
+                <input type="date" id="birthday" name="birthday" />
+              </div>
+              <div class="card-item card-item-registration flex-box-column">
+                <label>Gender</label>
+                <div class="radio-container">
+                  <label class="radio-item">
+                    <input type="radio" id="male"  name="gender" checked="checked" />
+                    Male
+                  </label>
+                  <label class="radio-item">
+                    <input type="radio" id="female"  name="gender" />
+                    Female
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="card-item-container card-item-container-registration">
+              <div class="card-item card-item-registration flex-box-column">
+                <label>Email</label
+                ><input type="text" id="email" name="email" placeholder="Your Email..." />
+              </div>
+              <div class="card-item card-item-registration flex-box-column">
+                <label>Phone Number</label
+                ><input type="text" id="phoneNumber" name="phoneNumber" placeholder="Your phone number..." />
+              </div>
+            </div>
+            <div class="card-item-container card-item-container-registration">
+              <div class="card-item card-item-registration flex-box-column">
+                <label for="">IC Number</label
+                ><input type="text" id="ic" name="ic" placeholder="Your IC Number..." />
+              </div>
+              <div class="card-item card-item-registration flex-box-column">
+                <label for="">password</label
+                ><input type="text" id="pass" name="pass"  placeholder="Your password..." />
+              </div>
+            </div>
+          <button type="submit" class="">Sign Up</button>
+          </form>
 
-          <div class="card-item-container card-item-container-registration">
-            <div class="card-item card-item-registration flex-box-column">
-              <label>Email</label
-              ><input type="text" id="email" name="email" placeholder="Your Email..." />
-            </div>
-            <div class="card-item card-item-registration flex-box-column">
-              <label>Phone Number</label
-              ><input type="text" id="phoneNumber" name="phoneNumber" placeholder="Your phone number..." />
-            </div>
-          </div>
-          <div class="card-item-container card-item-container-registration">
-            <div class="card-item card-item-registration flex-box-column">
-              <label for="">IC Number</label
-              ><input type="text" id="ic" name="ic" placeholder="Your IC Number..." />
-            </div>
-            <div class="card-item card-item-registration flex-box-column">
-              <label for="">password</label
-              ><input type="text" id="pass" name="pass"  placeholder="Your password..." />
-            </div>
-          </div>
-        <button type="submit" class="">Sign Up</button>
-
-        </form>
-      </section>
+      </div>
     </main>
     <footer></footer>
   </body>
